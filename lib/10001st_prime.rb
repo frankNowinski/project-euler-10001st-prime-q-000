60eb36bec8d940e46852a33e58b886bdb5742610
frankNowinski <nowinski.frank@gmail.com>
2016-06-18 16:00:33 -0400

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,14 @@
-# Implement your procedural solution here!
+def prime_number_for(input)
+  count = 0
+  number = 1
+
+  until count == input
+    number += 1
+    count += 1 if is_prime?(number)
+  end
+  number
+end
+
+def is_prime?(digit)
+  (2..Math.sqrt(digit).to_i).none?{ |i| digit % i == 0 }
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,23 @@
-# Implement your object-oriented solution here!
+class Prime
+  attr_reader :number
+
+  def initialize(input)
+    @input = input
+    prime_number_for
+  end
+
+  def prime_number_for
+    count = 0
+    @number = 1
+
+    until count == @input
+      @number += 1
+      count += 1 if is_prime?(number)
+    end
+    @number
+  end
+
+  def is_prime?(number)
+    (2..Math.sqrt(number).to_i).none?{ |i| number % i == 0 }
+  end
+end
